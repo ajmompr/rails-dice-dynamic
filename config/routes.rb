f0525d288611d8ac92c20e120e969672974e576d
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 16:02:45 +0000

    added dynamic dice route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "homepage"})
+  get("/dice/:number_of_dice/:sides", { :controller => "pages", :action => "flexible"})
 end
