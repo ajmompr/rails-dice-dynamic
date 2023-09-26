52433314f22886d01d65e99b2d44d31975d02b44
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 16:03:11 +0000

    defined flexible action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def homepage
     render({ :template => "pages_templates/home"})
   end
+
+  def flexible
+    render({ :template => "pages_templates/flexible"})
+  end
 end
